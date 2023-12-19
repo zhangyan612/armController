c1ae5a5d6b8ff40f37280dee597782211cf51734
Yan Zhang <zhangyan612@gmail.com>
2023-12-18 20:18:55 -0600

    arduino control motor in py

--- arduinoControl.py
+++ arduinoControl.py
@@ -1,14 +1,20 @@
 import time
 import serial
 
+# Write data to the serial port
+def send_command(command):
+    ser.write((command + '\n').encode())  # Send the command to the Arduino
+
 # Create a serial object
 ser = serial.Serial(
-    port='COM6',  # replace with your port
+    port='COM5',  # replace with your port
     baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
 )
+
+time.sleep(2)  # Add a delay to aviod data missing 
 
 # Check if the serial port is open
 if ser.isOpen():
@@ -16,18 +22,16 @@ if ser.isOpen():
 else:
     print("Failed to open serial port")
 
-# Write data to the serial port
 
+send_command('f')  # Send the 'forward' command
 
-ser.write(b'\x01')  # '1' in hexadecimal, forward
-# time.sleep(1)
+time.sleep(2)
 
-# ser.write(b'\x02')  # '2' in hexadecimal, backward
-time.sleep(1)
+send_command("b")  # Send the 'backward' command
 
-# ser.write(b'\x00')  # '0' in hexadecimal, stop
+time.sleep(2)
 
-# print("data sent")
+send_command("s")  # Send the 'forward' command
 
 # ser.flushInput()  # 清空接收缓存
 # portRead()  # 将单线串口配置为输入
@@ -37,16 +41,3 @@ if count != 0:  # 如果接收到的数据不空
     recv_data = ser.read(count)  # 读取接收到的数据
     print(recv_data)
 
-
-
-# 9740
-# 10000
-# 10260
-
-# 10520
-
-# 10260
-# 9740
-# 9480
-# 9220
-# 8960
