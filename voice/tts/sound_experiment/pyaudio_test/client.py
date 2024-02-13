a1f2f6ce697686bc572472017bba37fa874a77bd
Yan Zhang <zhangyan612@gmail.com>
2024-02-12 18:03:06 -0600

    update test

--- voice/tts/sound_experiment/pyaudio_test/client.py
+++ voice/tts/sound_experiment/pyaudio_test/client.py
@@ -2,6 +2,17 @@ import pyaudio
 import asyncio
 import websockets
 
+CHUNK = 1024 * 3
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
 
 def record_microphone(stream):
     CHUNK = 1024 *3
@@ -11,25 +22,6 @@ def record_microphone(stream):
 
 async def send_audio():
     async with websockets.connect('ws://localhost:8888') as ws:
-        p = pyaudio.PyAudio()
-        # obtain the index of available mic
-        mic_device_index = None
-        for i in range(p.get_device_count()):
-            device_info = p.get_device_info_by_index(i)
-            if device_info['maxInputChannels'] > 0:
-                mic_device_index = i
-                break
-
-        if mic_device_index is None:
-            print("there is no mic")
-            return
-
-        stream = p.open(format=pyaudio.paInt16,
-                        channels=1,
-                        rate=16000,
-                        input=True,
-                        frames_per_buffer=1024 *3,
-                        input_device_index=mic_device_index)
 
         for data in record_microphone(stream):
             await ws.send(data)
