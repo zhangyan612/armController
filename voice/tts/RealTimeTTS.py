bf9c88a830d7ccdb6fb09ac1acc68bacafba9c29
Yan Zhang <zhangyan612@gmail.com>
2024-02-14 01:54:25 -0600

    Working well

--- voice/tts/RealTimeTTS.py
+++ voice/tts/RealTimeTTS.py
@@ -1,10 +1,10 @@
-import pyaudio
-import webrtcvad
-import numpy as np
-import os
 from faster_whisper import WhisperModel
+import numpy as np
+import webrtcvad
+import pyaudio
 import threading
-import wave
+import time
+import os
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
@@ -16,12 +16,12 @@ class VoiceRecorder:
         self.CHUNK = 1024 * 3
         self.RATE = 16000
         self.model_size = 'base.en'
-        self.frames_np = None
         self.on_recording = False
-        self.frames_offset = 0.0
-        self.timestamp_offset = 0.0
-
+        # self.frames_offset = 0.0
+        # self.timestamp_offset = 0.0
+        self.silence_threshold = 5
         self.frames = []
+        self.transcribedText = '' 
         # Set up PyAudio
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format=pyaudio.paInt16,
@@ -31,18 +31,6 @@ class VoiceRecorder:
                         frames_per_buffer=self.CHUNK)
         
         self.model = WhisperModel(self.model_size, device="cpu", compute_type="int8")
-
-    def add_frames(self, frame_np):
-        '''
-            If the buffer size exceeds a threshold (45 seconds of audio data), it discards the oldest 30 seconds
-        '''
-        if self.frames_np is not None and self.frames_np.shape[0] > 45*self.RATE:
-            self.frames_offset += 30.0
-            self.frames_np = self.frames_np[int(30*self.RATE):]
-        if self.frames_np is None:
-            self.frames_np = frame_np.copy()
-        else:
-            self.frames_np = np.concatenate((self.frames_np, frame_np), axis=0)
 
     def _is_webrtc_speech(self, data, all_frames_must_be_true=False):
         sample_rate = self.RATE
@@ -69,26 +57,19 @@ class VoiceRecorder:
         audio = np.frombuffer(buffer=input_audio, dtype=np.int16)
         # Convert s16 back to f32.
         audio = audio.astype(np.float32) / 32768.0
-
+        
+        start = time.time()
         segments, info = self.model.transcribe(audio, beam_size=5, vad_filter=True, vad_parameters={"threshold": 0.5})
         print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
         for segment in segments:
             print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+            self.transcribedText+=(' '+segment.text)
+        infer_time = time.time() - start
 
-    def write_audio_frames_to_file(self, frames, file_name):
-        with wave.open(file_name, "wb") as wavfile:
-            wavfile: wave.Wave_write
-            wavfile.setnchannels(1)
-            wavfile.setsampwidth(2)
-            wavfile.setframerate(self.RATE)
-            wavfile.writeframes(frames)
-
-    def transcribe_from_File(self, frames, file_name):
-        self.write_audio_frames_to_file(frames, file_name)
-        self.transcribe(file_name) # need to remove conversion
+        print(f"Detection done in {infer_time} seconds")
 
     def recording(self):
-        n_audio_file = 0
+        silence_start_time = None
 
         while True:
             # Read chunk of audio data
@@ -98,12 +79,8 @@ class VoiceRecorder:
                 print("Voice detected!")
                 # add data to frames
                 self.frames.append(data)
-
-                # works too
-                # raw_data = np.frombuffer(buffer=data, dtype=np.int16)
-                # raw_data = raw_data.astype(np.float32) / 32768.0
-                # self.add_frames(raw_data)
                 self.on_recording = True
+                silence_start_time = None  # reset the silence timer
             else:
                 if self.on_recording:
                     print("Voice ended, transcribing...")
@@ -118,24 +95,19 @@ class VoiceRecorder:
                     )
                     t.start()
 
-                    # save to file
-                    # t = threading.Thread(
-                    #     target=self.transcribe_from_File,
-                    #     args=(
-                    #         audio_data,
-                    #         f"chunks/{n_audio_file}.wav",
-                    #     ),
-                    # )
-                    # t.start()
-                    # n_audio_file += 1
-
                     # clear frames
                     self.frames = []
-                    # self.frames_np = None
                     self.on_recording = False
+                    silence_start_time = time.time()  # start the silence timer
                 else:
-                    print("No voice detected.")
+                    # print("No voice detected.")
                     # if no voice detected for 5 seconds, print whole setense
+                    if silence_start_time and time.time() - silence_start_time > self.silence_threshold:  # 5 seconds of silence
+                        if self.transcribedText:
+                            print("%s seconds of silence detected. Send transcribed text." % (self.silence_threshold))
+                            print(self.transcribedText)
+                            self.transcribedText = ''  # reset the transcribed text
+                        silence_start_time = time.time()  # reset the silence timer
 
 
 if __name__ == "__main__":
