ec5c6955cedd92e0e52467d8fca35809cf038e97
Yan Zhang <zhangyan612@gmail.com>
2024-02-19 00:19:07 -0600

    update move

--- arm/PythonSDK/MovecExample.py
+++ arm/PythonSDK/MovecExample.py
@@ -4,6 +4,8 @@ import time
 # Initialize the robot with specified host and port
 robot = STR400(host='localhost', port=8080)
 
+defaultAngles= [0, -90, 0, 0, 0, 0, 5]
+
 # Activating the robot
 robot.enable()
 print("Robot successfully activated.")
@@ -11,7 +13,7 @@ print("Robot successfully activated.")
 # Commanding the robot to move its joints to specified angles using MoveJ
 print(
     "Initiating MoveJ to target angles [0, 0, 0, 0, 0, 0] with a duration of 6 seconds...")
-angles = [0, 0, 0, 0, 0, 0, 6]
+angles = defaultAngles
 robot.movej(angles)
 
 # Brief pause to ensure the MoveJ command is processed
@@ -30,9 +32,17 @@ while True:
 time.sleep(5)
 
 # Commanding the robot to move to a specified Cartesian pose using MoveC
-CartesianPose = [-41, 83, 603, 19, -26, -126, 10]
+# CartesianPose = [-190, -400, 200, 90, -30, -90, 10]
+# robot.movec(CartesianPose)
+# time.sleep(0.5)
+
+CartesianPose = [-325, -125, 130, 98, -5, 25, 10]
 robot.movec(CartesianPose)
 time.sleep(0.5)
+
+# angles = [45, -85, 0, 0, 0, 0, 10]
+# robot.movej(angles)
+
 
 print("Monitoring task status until the MoveC operation is completed...")
 while True:
@@ -48,7 +58,7 @@ time.sleep(5)
 # Repeating MoveJ command to return to the initial joint angles
 print(
     "Initiating MoveJ back to initial angles [0, 0, 0, 0, 0, 0] with a duration of 6 seconds...")
-angles = [0, 0, 0, 0, 0, 0, 6]
+angles = defaultAngles
 robot.movej(angles)
 
 # Brief pause to allow processing of the MoveJ command
