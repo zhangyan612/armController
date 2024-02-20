ec30e6b48eb4175047ba7ac3145ef830e978e85c
Yan Zhang <zhangyan612@gmail.com>
2024-02-19 21:31:17 -0600

    add script for auto run

---
+++ scripts/runTTS.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+echo "Running tts"
+
+/home/wheeltec/archiconda3/envs/whisper/bin/python /home/wheeltec/armController/voice/pipeline/edge_TTS.py
---
+++ scripts/runVoice.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+echo "Running voice"
+
+/home/wheeltec/archiconda3/envs/whisper/bin/python /home/wheeltec/armController/voice/pipeline/main.py
