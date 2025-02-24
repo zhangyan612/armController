bcd976efb56e5f7bd0ad989ce866d39aeb81140a
Yan Zhang <zhangyan612@gmail.com>
2025-02-24 12:07:51 -0600

    can calibrate

--- odriveControl/STW_motor_demo_TB.py
+++ odriveControl/STW_motor_demo_TB.py
@@ -311,12 +311,20 @@ def main():
     bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate=500000) 
     #bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate=1000000) 
     
-    motorID1 = 0x01                      
-    calibration(bus,motorID1)             # 执行校准
+    # motorID1 = 0x01                      
+    # calibration(bus,motorID1)             # 执行校准
 
-    motorID1 = 0x02                       
-    calibration(bus,motorID1)             # 执行校准
+    # motorID2= 0x02                       
+    # calibration(bus,motorID2)             # 执行校准
 
+    # motorID3 = 0x03                       
+    # calibration(bus,motorID3)             # 执行校准 3.7 old hardware
+
+    # motorID4 = 0x04                       
+    # calibration(bus,motorID4)             # 执行校准
+
+    motorID5 = 0x05                       
+    calibration(bus,motorID5)             # 执行校准 can disabled not working  3.7 old hardware
 
 
     # 在退出时确保资源被释放  
